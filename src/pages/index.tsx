a560cb7ad9512e376e7d552397578d45cdfe3642
Juan carlos <juancarloscruz278@gmail.com>
2021-02-11 18:08:40 -0400

    feat: installs typescript config

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Yelp app</title>
+                <link rel='icon' href='/favicon.ico' />
+            </Head>
+        </div>
+    );
+}
