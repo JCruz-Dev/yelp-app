23c8576ae460ea5565b757f6e96cdaa97cac6893
Juan carlos <juancarloscruz278@gmail.com>
2021-02-15 02:22:33 -0400

    feat: configure aliases and installs next svg dependency

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
-module.exports = {
-    async rewrites() {
-      return [
-        {
-          source: '/api/yelp',
-          destination: 'https://api.yelp.com/v3/graphql'
-        }
-      ];
-    }
-  };
+const withReactSvg = require('next-react-svg')
+const path = require('path')
+ 
+module.exports = withReactSvg({
+  include: path.resolve(__dirname, 'src/assets/svg'),
+  webpack(config) {
+    return config
+  }
+},{
+  async rewrites() {
+    return [
+      {
+        source: '/api/yelp',
+        destination: 'https://api.yelp.com/v3/graphql'
+      }
+    ];
+  }
+})
