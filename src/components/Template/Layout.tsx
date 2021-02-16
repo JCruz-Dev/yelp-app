114d77491a0fd2fcc2c8cda3e9ca8e58d7c97fa5
Juan carlos <juancarloscruz278@gmail.com>
2021-02-15 23:44:11 -0400

    fix: refactor layout, header and form stlying

--- src/components/Template/Layout.tsx
+++ src/components/Template/Layout.tsx
@@ -3,7 +3,7 @@ import Header from '@Molecules/Header'
 const Layout: React.FC = ({ children }): JSX.Element => {
   return (
     <div className="layout__container">
-      <div className="layout__content container">
+      <div className="container layout__content">
         <Header />
         <div className="layout__items">{children}</div>
       </div>
