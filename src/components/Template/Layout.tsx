7564cf01c3e3b9f508ee20deca8cdd86122735ce
Juan carlos <juancarloscruz278@gmail.com>
2021-02-17 05:12:05 -0400

    fi: refactors layout component to avoid re-renders

--- src/components/Template/Layout.tsx
+++ src/components/Template/Layout.tsx
@@ -1,13 +1,21 @@
 import Header from '@Molecules/Header'
+import { useMemo } from 'react'
 
 const Layout: React.FC = ({ children }): JSX.Element => {
   return (
-    <div className="layout__container">
-      <div className="container layout__content">
-        <Header />
-        <div className="layout__items">{children}</div>
-      </div>
-    </div>
+    <>
+      {useMemo(
+        () => (
+          <div className="layout__container">
+            <div className="container layout__content">
+              <Header />
+              <div className="layout__items">{children}</div>
+            </div>
+          </div>
+        ),
+        [children]
+      )}
+    </>
   )
 }
 export default Layout
