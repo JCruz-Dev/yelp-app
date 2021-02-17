f8b1848c4edc52f6c1a14dce560df328a604f732
Juan carlos <juancarloscruz278@gmail.com>
2021-02-17 05:13:00 -0400

    fix: refactors index page to be simplified.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
-import { useState, useRef } from 'react'
+import { useState, useCallback } from 'react'
 import Head from 'next/head'
 import { NextPage } from 'next'
 import { useLazyQuery } from '@apollo/client'
-import Link from 'next/link'
-import { EventForm, AppDispatch, businessState, userSearch } from '@PTypes'
+import { AppDispatch, businessState, userSearch } from '@PTypes'
 import { useDispatch, useSelector } from 'react-redux'
 import { saveData } from '@ReduxSlices/userDataReducer'
 import { SEARCH_QUERY } from '@Queries'
@@ -11,15 +10,18 @@ import Layout from '@Templates/Layout'
 import Title from '@Atoms/Title'
 import Paragraph from '@Atoms/Paragraph'
 import CardLoader from '@Templates/CardLoader'
+import EmptyMessage from '@Templates/EmptyMessage'
+import SearchForm from '@Molecules/SearchForm'
+import BusinessContainer from '@Organisms/BusinessContainer'
+import ErrorMessage from '@Templates/ErrorMessage'
+
 const Home: NextPage = () => {
   //dispatch action
   const dispatch = useDispatch<AppDispatch>()
-  //Get state from store
-  const state: Array<businessState> = useSelector((state: userSearch) => state.userData.queryData)
-  //Use Ref to get input value
-  const inputRef = useRef(null)
   //State foe store user input
   const [search, setSearch] = useState('')
+  //Get state from store
+  const state: Array<businessState> = useSelector((state: userSearch) => state.userData.queryData)
   //Query Data
   const [loadInput, { loading, data }] = useLazyQuery(SEARCH_QUERY, {
     variables: {
@@ -30,13 +32,12 @@ const Home: NextPage = () => {
       dispatch(saveData(data.search.business))
     },
   })
-  const handleSubmit = (e: EventForm): void => {
-    e.preventDefault()
-    if (inputRef.current.value !== '') {
-      setSearch(inputRef.current.value)
-      loadInput()
-    }
-  }
+  const handleSubmit = useCallback(
+    (search: string): void => {
+      setSearch(search)
+    },
+    [setSearch]
+  )
   return (
     <div>
       <Head>
@@ -46,40 +47,14 @@ const Home: NextPage = () => {
       <Layout>
         <Title titleName="Welcome to Yelpi App" Tag="h1" />
         <Paragraph name="The best place to search for a business" classes="search__paragram" />
-        <form onSubmit={handleSubmit} className="form">
-          <input
-            type="text"
-            name="search"
-            id="search"
-            placeholder="Search for food, drinks and much more."
-            ref={inputRef}
-            className="form__input"
-            required
-          />
-          <button type="submit" className="form__button">
-            Search
-          </button>
-        </form>
+        <SearchForm callLazyQuery={loadInput} onSubmit={handleSubmit} />
       </Layout>
       <div className="container">
-        <CardLoader />
-        {/* {loading ? (
-          <p>Loading....</p>
-        ) : (
-          state &&
-          state.map((d) => (
-            <Link href={`/business/${d.id}`} key={d.id}>
-              <a>
-                <p>
-                  {d.name} {d.viewed && <span>[visto]</span>}
-                </p>
-              </a>
-            </Link>
-          ))
+        {state.length === 0 && !loading && data && data.search.business.length !== 0 && (
+          <EmptyMessage />
         )}
-        {data && data.search.business.length <= 0 && (
-          <p>We could not find anything related to your search :(</p>
-        )} */}
+        <div className="card-grid">{loading ? <CardLoader /> : <BusinessContainer />}</div>
+        {data && data.search.business.length <= 0 && <ErrorMessage />}
       </div>
     </div>
   )
