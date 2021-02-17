d633754dc78b734859ff330ac8b465bf0645a090
Juan carlos <juancarloscruz278@gmail.com>
2021-02-17 05:06:31 -0400

    fix: adds new domains for images to be used on next image component

--- next.config.js
+++ next.config.js
@@ -15,5 +15,8 @@ module.exports = withPlugins([[ withReactSvg,{
         destination: 'https://api.yelp.com/v3/graphql'
       }
     ];
+  },
+  images: {
+    domains: ['s3-media1.fl.yelpcdn.com','s3-media2.fl.yelpcdn.com', 's3-media3.fl.yelpcdn.com', 's3-media4.fl.yelpcdn.com']
   }
 })
