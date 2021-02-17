b90058bfd6d76d3da890b69d20f6f5ddb027672b
Juan carlos <juancarloscruz278@gmail.com>
2021-02-17 05:05:35 -0400

    fix: add useMemo to prevent re-rendering

--- src/components/Atoms/Icon.tsx
+++ src/components/Atoms/Icon.tsx
@@ -1,8 +1,9 @@
 import dynamic from 'next/dynamic'
 import { SvgProp } from '@PTypes'
+import { useMemo } from 'react'
 
 const Icon: React.FC<SvgProp> = ({ svgIcon }): JSX.Element => {
   const DynamicIcon = dynamic(() => import(`@Assets/svg/${svgIcon}.svg`).catch(() => false))
-  return <>{DynamicIcon ? <DynamicIcon /> : null}</>
+  return <>{useMemo(() => (DynamicIcon ? <DynamicIcon /> : null), [DynamicIcon])}</>
 }
 export default Icon
