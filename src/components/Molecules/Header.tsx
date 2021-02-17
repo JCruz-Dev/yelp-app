eae0c5c85595f19be73b6a95208a0981158c47e5
Juan carlos <juancarloscruz278@gmail.com>
2021-02-17 05:08:58 -0400

    fix: refactors header component to avoid re-render

--- src/components/Molecules/Header.tsx
+++ src/components/Molecules/Header.tsx
@@ -1,11 +1,19 @@
 import Icon from '@Atoms/Icon'
 import LogoImage from '@Atoms/Logo'
+import { useMemo } from 'react'
 const Header: React.FC = (): JSX.Element => {
   return (
-    <header className="header__content">
-      <LogoImage />
-      <Icon svgIcon="menu-icon" />
-    </header>
+    <>
+      {useMemo(
+        () => (
+          <header className="header__content">
+            <LogoImage />
+            <Icon svgIcon="menu-icon" />
+          </header>
+        ),
+        []
+      )}
+    </>
   )
 }
 export default Header
