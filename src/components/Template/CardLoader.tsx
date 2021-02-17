e871a027d882811f433d8983712531e91b0314fe
Juan carlos <juancarloscruz278@gmail.com>
2021-02-17 05:11:38 -0400

    fix: refactor card loader component to render 3 separed skeletons

--- src/components/Template/CardLoader.tsx
+++ src/components/Template/CardLoader.tsx
@@ -1,15 +1,17 @@
 const CardLoader: React.FC = () => {
   return (
     <>
-      <div className="loader">
-        <div className="loader__content">
-          <div className="loader__img"></div>
-          <div className="loader__title"></div>
-          <div className="loader__comments"></div>
-          <div className="loader__direction"></div>
-          <div className="loader__phone"></div>
+      {[...Array(3)].map((value: undefined, index: number) => (
+        <div className="loader" key={index}>
+          <div className="loader__content">
+            <div className="loader__img"></div>
+            <div className="loader__title"></div>
+            <div className="loader__comments"></div>
+            <div className="loader__direction"></div>
+            <div className="loader__phone"></div>
+          </div>
         </div>
-      </div>
+      ))}
     </>
   )
 }
