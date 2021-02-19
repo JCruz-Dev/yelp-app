8657db2b0faac657bcb578619a1559539f1ebc2b
Juan carlos <juancarloscruz278@gmail.com>
2021-02-19 19:55:19 -0400

    fix: refactor Business loader

--- src/components/Template/BusinessLoader.tsx
+++ src/components/Template/BusinessLoader.tsx
@@ -1,20 +1,32 @@
 const BusinessLoader: React.FC = (): JSX.Element => {
   return (
-    <div className="business__loader__container">
-      <div className="container">
-        <div className="business__loader">
-          <div className="business__loader__info">
-            <div className="business__loader__info__title"></div>
-            <div className="business__loader__info__reviews"></div>
-            <div className="business__loader__info__location"></div>
-            <div className="business__loader__info__phone"></div>
-            <div className="business__loader__info__price"></div>
-            <div className="business__loader__info__button"></div>
+    <>
+      <div className="business__loader__container">
+        <div className="container">
+          <div className="business__loader">
+            <div className="business__loader__info">
+              <div className="business__loader__info__title"></div>
+              <div className="business__loader__info__reviews"></div>
+              <div className="business__loader__info__location"></div>
+              <div className="business__loader__info__phone"></div>
+              <div className="business__loader__info__price"></div>
+              <div className="business__loader__info__button"></div>
+            </div>
+            <div className="business__loader__image"></div>
           </div>
-          <div className="business__loader__image"></div>
         </div>
       </div>
-    </div>
+      <div className="container business__secondary">
+        <article>
+          <h2 className="business__secondary__card__title">Hours</h2>
+          <div className="loader__item__hour"></div>
+        </article>
+        <article>
+          <h2 className="business__secondary__card__title">Reviews</h2>
+          <div className="loader__item__review"></div>
+        </article>
+      </div>
+    </>
   )
 }
 export default BusinessLoader
