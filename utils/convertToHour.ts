0e7b39c2fbfe345ff8a076e5a89d72dd072235c0
Juan carlos <juancarloscruz278@gmail.com>
2021-02-20 03:30:54 -0400

    fix: refactor conver to hour function

--- utils/convertToHour.ts
+++ utils/convertToHour.ts
@@ -3,5 +3,5 @@ export function convertToHour(item: string): string {
   const minutes = item.substring(2, item.length)
   const convertedHours = parseInt(hour) % 12 || 12
   const amOrPm = parseInt(hour) >= 12 ? 'pm' : 'am'
-  return `${convertedHours}:${minutes} ${amOrPm}`
+  return `${('0' + convertedHours).slice(-2)}:${minutes} ${amOrPm}`
 }
